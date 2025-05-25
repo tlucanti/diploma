bf704ea17ef5586c63eddcc5f97efe068dacb326
Konstantin Semichastnov <tlucanti@yandex-team.ru>
2025-05-25 13:53:51 +0300

    add build script
    Signed-off-by: Konstantin Semichastnov <tlucanti@yandex-team.ru>

---
+++ maga/build.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from markdown_pdf import MarkdownPdf, Section
+
+pdf = MarkdownPdf(toc_level=4, optimize=True)
+
+for fname in [
+    "intro.md",
+    "ch1.md",
+    "ch2.md",
+    "ch3.md",
+    "ch4.md",
+    "conclusion.md",
+]:
+    with open(fname) as f:
+        print(f"adding {fname}")
+        pdf.add_section(Section(f.read()))
+pdf.meta["title"] = "diploma"
+print("saving as diploma.pdf")
+pdf.save("diploma.pdf")
