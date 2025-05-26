d923ce917be3b787addb27e670feaff3c76f617a
Konstantin Semichastnov <tlucanti@yandex-team.ru>
2025-05-26 18:56:58 +0300

    update arch script
    Signed-off-by: Konstantin Semichastnov <tlucanti@yandex-team.ru>

--- maga/arch/a.sh
+++ maga/arch/a.sh
@@ -2,10 +2,17 @@
 
 max_hashes=${1:-2}
 
-if ! [[ "$max_hashes" =~ ^[0-9]+$ ]] || [ "$max_hashes" -eq 0 ]; then
-  echo "Usage: $0 [positive_integer]"
-  exit 1
-fi
+function arch() {
+  if ! [[ "$max_hashes" =~ ^[0-9]+$ ]] || [ "$max_hashes" -eq 0 ]; then
+    echo "Usage: $0 [positive_integer]"
+    exit 1
+  fi
+  cat $1 | grep -E "^[[:space:]]*#{1,$max_hashes} "
+}
 
-cat arch.md | grep -E "^[[:space:]]*#{1,$max_hashes} " > a.md
+arch ch1.md > arch/a.md
+arch ch2.md >> arch/a.md
+arch ch3.md >> arch/a.md
+arch ch4.md >> arch/a.md
+arch conclusion.md >> arch/a.md
 
