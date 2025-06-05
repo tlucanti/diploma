d24b94dc057909c522f1d4382711f3e3d6de9946
Konstantin Semichastnov <tlucanti@yandex-team.ru>
2025-06-05 22:16:20 +0300

    fix scripts
    Signed-off-by: Konstantin Semichastnov <tlucanti@yandex-team.ru>

--- maga/arch/a.sh
+++ maga/arch/a.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-max_hashes=${1:-2}
-
 function arch() {
+  max_hashes=${2:-2}
+
   if ! [[ "$max_hashes" =~ ^[0-9]+$ ]] || [ "$max_hashes" -eq 0 ]; then
-    echo "Usage: $0 [positive_integer]"
+    echo "Usage: $0 [positive_integer]" >&2
     exit 1
   fi
   cat $1 | grep -E "^[[:space:]]*#{1,$max_hashes} "
--- maga/st.sh
+++ maga/st.sh
@@ -8,12 +8,13 @@ function frag() {
 }
 
 if [ -z "$1" ]; then
-	echo "expected argument";
+	echo "expected argument" >&2
 	exit 1;
 fi
 
-arch ./ch1.md > structure.md
-arch ./ch2.md >> structure.md
+arch ./ch1.md 4 > structure.md
+arch ./ch2.md 4 >> structure.md
 #cat ./CHAPTER-2.md >> structure.md
-frag ./ch3.md ./CHAPTER-3.md $1 >> structure.md
-arch ./ch4.md >> structure.md
+#frag ./ch3.md ./CHAPTER-3.md $1 >> structure.md
+arch ./CHAPTER-3.md 4 >> structure.md
+frag ./ch4.md ./CHAPTER-4.md $1 >> structure.md
